1954be1f16c6297a5e9392be74a46b4e9081bcff
Pablo Barbero <pablobarbero220@gmail.com>
2023-01-29 17:53:03 -0300

    Add function to change quantity

--- src/components/Cart/CartProduct.tsx
+++ src/components/Cart/CartProduct.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Box, Divider, Grid } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Divider, Grid, NativeSelect } from "@mui/material";
 import "./CartProduct.css";
 import { CartProductProps } from "../assets/products-JSON";
 
@@ -9,7 +9,46 @@ export const CartProduct: React.FC<CartProductProps> = ({
   content,
   price,
   image,
+  index,
+  update,
 }) => {
+  const [newQuantity, setNewQuantity] = useState<number>(Number(quantity));
+
+  const handleNewQuantity = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    e.preventDefault();
+
+    setNewQuantity(Number(e.target.value));
+    update((prevState: Array<Object>) =>
+      prevState.map((item, ind) => {
+        return ind === index ? { ...item, quantity: e.target.value } : item;
+      })
+    );
+  };
+
+  const quantityHandler = () => {
+    return (
+      <Box>
+        <NativeSelect
+          defaultValue={quantity}
+          inputProps={{
+            name: "age",
+            id: "uncontrolled-native",
+          }}
+          onChange={handleNewQuantity}
+        >
+          {quantity > 0 &&
+            [...Array(quantity)].map((max: number, index: number) => {
+              return (
+                <option key={index + "quantity"} value={index + 1}>
+                  {index + 1}
+                </option>
+              );
+            })}
+        </NativeSelect>
+      </Box>
+    );
+  };
+
   return (
     <Box className="cart-product">
       <Box className="cart-product-image">
@@ -19,7 +58,7 @@ export const CartProduct: React.FC<CartProductProps> = ({
 
       <Box className="cart-product-info">
         <h3>{name}</h3>
-        <h4>Quantity: {quantity}</h4>
+        <h4>Quantity: {quantityHandler()}</h4>
         <ul>
           {content?.length &&
             content.map((item, index) => (
@@ -37,7 +76,7 @@ export const CartProduct: React.FC<CartProductProps> = ({
 
       <Box>
         <h3>${price}</h3>
-        <h3>Total: ${quantity * price}</h3>
+        <h3>Total: ${(newQuantity * price).toFixed(2)}</h3>
       </Box>
     </Box>
   );
